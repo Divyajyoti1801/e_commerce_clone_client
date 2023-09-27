5d101a9a4dd0d6c28ea20525f54ed1638f02eadc
Divyajyoti <Divyajyoti182000@gmail.com>
2023-09-27 22:13:49 +0530

    Creating Pop Components

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Button from "@/components/ui/button";
+import IconButton from "@/components/ui/icon-button";
 import { Color, Size } from "@/types";
 import { Dialog } from "@headlessui/react";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { useState } from "react";
+import Filter from "./filter";
 
 interface MobileFilterProps {
   sizes: Size[];
@@ -32,7 +34,17 @@ const MobileFilters: React.FC<MobileFilterProps> = ({ sizes, colors }) => {
         <div className="fixed inset-0 bg-black bg-opacity-25" />
         {/* Dialog Position */}
         <div className="fixed inset-0 z-40 flex">
-          <Dialog.Panel className="rea"></Dialog.Panel>
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-5 shadow -xl">
+            {/* Close Button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<X size={15} />} onClick={onClose} />
+            </div>
+            {/* Render the filters */}
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+          </Dialog.Panel>
         </div>
       </Dialog>
     </>
