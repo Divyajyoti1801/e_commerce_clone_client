5bf97527cb6dc6c33502672831f26de36fb11190
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-09-27 11:58:33 +0530

    Saving_Docs hardware issue

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,4 +1,9 @@
+"use client";
+import Button from "@/components/ui/button";
 import { Color, Size } from "@/types";
+import { Dialog } from "@headlessui/react";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 interface MobileFilterProps {
   sizes: Size[];
@@ -6,7 +11,32 @@ interface MobileFilterProps {
 }
 
 const MobileFilters: React.FC<MobileFilterProps> = ({ sizes, colors }) => {
-  return <div>MobileFilter</div>;
+  const [open, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
+        Filters
+        <Plus size={20} />
+      </Button>
+      <Dialog
+        open={open}
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={onClose}
+      >
+        {/* Background */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+        {/* Dialog Position */}
+        <div className="fixed inset-0 z-40 flex">
+          <Dialog.Panel className="rea"></Dialog.Panel>
+        </div>
+      </Dialog>
+    </>
+  );
 };
 
 export default MobileFilters;
